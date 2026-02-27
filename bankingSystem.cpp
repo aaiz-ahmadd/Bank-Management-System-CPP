108ba5774bd8b5d62ac0b6f9691ecac0b3333cbd
Aaiz Ahmad <aaiz_ahmadd@Aaizs-MacBook-Pro.local>
2026-02-28 00:34:10 +0500

    Transaction History Added

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+struct Transactions{
+    char type[20];
+    double amount;
+    double balance_after;
+};
+
 void copyArray(char dest[], char src[]){
     int i = 0;
     while(src[i] != '\0'){
@@ -15,7 +21,11 @@ class BankAccount{
     int account_number;
     char owner_name[50];
     double balance;
+    Transactions transactions[50];
+    int transaction_counter;
+    int transaction_capacity;
     public:
+        BankAccount() : transaction_counter{0}, transaction_capacity{50}, balance{0} {}
         void setAccount(int acc_no, char owner[], double bal){
             account_number = acc_no;
             copyArray(owner_name, owner);
@@ -24,6 +34,12 @@ class BankAccount{
         int deposit(double cash){
             if(cash >= 0){
                 balance += cash;
+                if(transaction_counter < transaction_capacity){
+                    copyArray(transactions[transaction_counter].type, "Deposit");
+                    transactions[transaction_counter].amount = cash;
+                    transactions[transaction_counter].balance_after = balance;
+                    transaction_counter++;
+                }
                 return 1;
             }
             if(cash < 0)
@@ -33,6 +49,12 @@ class BankAccount{
         int withdraw(double cash){
             if(balance >= cash){
                 balance -= cash;
+                if(transaction_counter < transaction_capacity){
+                    copyArray(transactions[transaction_counter].type, "Withdraw");
+                    transactions[transaction_counter].amount = cash;
+                    transactions[transaction_counter].balance_after = balance;
+                    transaction_counter++;
+                }
                 return 1;
             }
             if(balance < 0)
@@ -47,6 +69,23 @@ class BankAccount{
         }
         char* getOwnerName(){
             return owner_name;
+        }
+        void displayTransactionHistory(){
+            cout << "Transaction History of Account " << this->account_number << "." << endl;
+            cout << "Transaction Type\tAmount\tBalance After\n";
+            for(int i = 0; i < transaction_counter; i++){
+                cout << transactions[i].type << "\t" << transactions[i].amount << "\t" << transactions[i].balance_after << endl;
+            }
+        }
+        void displayLastKTransactions(int k){
+            if(k > transaction_counter)
+                k = transaction_counter;
+            
+            cout << "Last " << k << " Transactions of Account " << this->account_number << "." << endl;
+            cout << "Transaction Type\tAmount\tBalance After\n";
+            for(int i = transaction_counter-k; i < transaction_counter; i++){
+                cout << transactions[i].type << "\t" << transactions[i].amount << "\t" << transactions[i].balance_after << endl;
+            }
         }
         void display(){
             cout << owner_name << "\t" << account_number << "\t" << balance << endl;
@@ -113,10 +152,28 @@ class Bank{
             int rec_acc = findAccount(to_acc);
             if(rec_acc == -1)
                 return 0;
+            if(sender_acc == rec_acc)
+                return 0;
             if(!withdrawFromAccount(from_acc, cash))
                 return 0;
             depositToAccount(to_acc, cash);
             return 1;
+        }
+        void displayTransactions(int acc_number){
+            int idx = findAccount(acc_number);
+            if(idx != -1){
+                accounts[idx].displayTransactionHistory();
+            }
+            else
+                cout << "Account not found!" << endl;
+        }
+        void displayKTransactions(int acc_no, int k){
+            int idx = findAccount(acc_no);
+            if(idx != -1){
+                accounts[idx].displayLastKTransactions(k);  
+            }
+            else
+                cout << "Account not found!" << endl;
         }
         void displayAllAccounts(){
             for(int i = 0; i < total_accounts; i++){
@@ -135,6 +192,8 @@ void menu(){
     cout << "3. Withdraw\n";
     cout << "4. Transfer\n";
     cout << "5. Display All Accounts\n";
+    cout << "6. Display Transaction Histor\n";
+    cout << "7. Last k Transactions\n";
     cout << "0. Exit\n";
     cout << "Enter your choice: ";
 }
@@ -226,6 +285,19 @@ int main(){
         else if(choice == 5){
             cout << "Displaying all Accounts: " << endl;
             bank.displayAllAccounts();
+        }
+        else if(choice == 6){
+            cout << "Enter Account No: ";
+            cin >> acc_number;
+            bank.displayTransactions(acc_number);
+        }
+        else if(choice == 7){
+            int k;
+            cout << "Enter Account No: ";
+            cin >> acc_number;
+            cout << "Enter Number of Transactions you want: ";
+            cin >> k;
+            bank.displayKTransactions(acc_number, k);
         }
         else if(choice == 0){
             cout << "Exiting....!" << endl;
